844f7cf344b5567c6611a8c706591ba48ce14948
ganeugen <ganeugen@gmail.com>
2022-10-15 19:42:10 +0200

    Excluded unnecessary classes

--- ClientSauto.py
+++
@@ -1,8 +0,0 @@
-import xmlrpc.client
-
-class ClientSauto:
-    def __init__(self):
-        self.__proxy = xmlrpc.client.ServerProxy('https://import.sauto.cz/RPC2')
-        print(self.__proxy.getHash('ganeugen@gmail.com'))
-        #self.__proxy.login()
-
